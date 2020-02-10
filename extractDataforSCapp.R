acaa8cece45309d3a63f5bb34b6ab5fc2994a824
marybecker <mary.becker@ct.gov>
2020-02-10 17:46:55 -0500

    added data

--- extractDataforSCapp.R
+++ extractDataforSCapp.R
@@ -1,9 +1,10 @@
-setwd("C:/Users/beckerm/Desktop/TrailCam2017")
+setwd("P:/Projects/GitHub_Prj/streamConnectViz")
 
 library(stringr)
+library(jsonlite)
 
-obs<-read.csv("TrailCam_FlowObs2017.csv",header=TRUE,stringsAsFactors = FALSE)
-files<-read.csv("imgfiles2017_attributeinfo.csv",header=TRUE,stringsAsFactors=FALSE)
+obs<-read.csv("data/TrailCam_FlowObs2017.csv",header=TRUE,stringsAsFactors = FALSE)
+files<-read.csv("data/imgfiles2017_attributeinfo.csv",header=TRUE,stringsAsFactors=FALSE)
 colnames(files)[4]<-"STA_SEQ"
 
 data<-merge(files,obs,by=c("STA_SEQ","Month","Day","Year"))
@@ -29,3 +30,13 @@ data<-merge(data,minTime16,by=c("STA_SEQ","Date","minute"))
 
 check<-aggregate(data$minute,by=as.list(data[,c("STA_SEQ","Date")]),FUN=length)
 check[check$x>1,]
+
+test_data<-data[data$STA_SEQ==16046|data$STA_SEQ==15244|data$STA_SEQ==19657,]
+test_data<-test_data[test_data$Month==8&test_data$Day<15,]
+test_data$relpath<-paste0("image/",test_data$fpath)
+
+imgPath<- test_data[,c(1,2,22)]
+imgPathJSON<-toJSON(imgPath,pretty=TRUE)
+write_json(imgPathJSON,"data/imgPath.json")
+
+##file.copy(test_data$pathtofile,'P:/Projects/GitHub_Prj/streamConnectViz/images')
