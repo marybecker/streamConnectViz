e86642ca4a37f954dc6ab5df530e880f66909722
Julia Golin <jgolin129@gmail.com>
2020-04-15 17:45:25 -0400

    gets current date on slider

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ d3.dsv(',','data/imgCat.csv', function(d) {
         console.log(data);
         console.log(sites);
 
-        for(let j = 0; j < sites.features.length; j++) {
+        /*for(let j = 0; j < sites.features.length; j++) {
             sites.features[j].properties.dates = {};
         }
         for(let i = 0; i < data.length; i++) {
@@ -20,14 +20,13 @@ d3.dsv(',','data/imgCat.csv', function(d) {
                     sites.features[j].properties.dates[data[i].date] = data[i].cat;
                 }
             }
-        }
+        }*/
 
         // add the jQuery slider
         $(function() {
             let minDate = new Date('2017-07-02');
             let maxDate = new Date('2017-11-1');
-
-            $( "#slider" ).slider({
+            $("#slider").slider({
                 min: minDate.getTime(),
                 max: maxDate.getTime(),
                 slide: function(event, ui) {
@@ -70,10 +69,34 @@ d3.dsv(',','data/imgCat.csv', function(d) {
             let popup = feature.properties.Station_Name;
             layer.bindPopup("<h3>"+ popup + "</h3>");
 
-            /*let myIcon = L.icon({
-                iconUrl: 'https://unpkg.com/leaflet@1.6.0/dist/images/marker-icon-2x.png';
+            // combine csv dates and category data with each site
+            for(let j = 0; j < sites.features.length; j++) {
+                sites.features[j].properties.dates = {};
             }
-            L.marker({icon: myIcon}).addTo(map);*/
+            for(let i = 0; i < data.length; i++) {
+                for(let j = 0; j < sites.features.length; j++) {
+                    // ----- join here -----
+                    if(data[i].seq == sites.features[j].properties.STA_SEQ) {
+                        sites.features[j].properties.dates[data[i].date] = data[i].cat;
+                    }
+                }
+            }
+
+            // attempt to match date from csv with current date on slider
+            for(let i = 0; i < data.length; i++) {
+                let getDate = new Date(data[i].date);
+                let newDate = getDate.toDateString();
+
+                // get current date of slider
+                $("#slider").bind("slidechange",function(event, ui) {
+                    let sliderDate = new Date(ui.value);
+                    //console.log(sliderDate.toDateString());
+
+                    if(sliderDate.toDateString() == newDate) {
+                        console.log('Date is ' + newDate);
+                    }
+                });
+            }
         }
 
         geojson = L.geoJson(sites, {
