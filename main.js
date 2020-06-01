6d4e6f5dec47fc8f361e948f1af5676c98ff9837
Julia Golin <jgolin129@gmail.com>
2020-06-01 17:11:53 -0400

    creating functions, still working

--- main.js
+++ main.js
@@ -10,14 +10,8 @@ d3.dsv(',','data/imgCat.csv', function(d) {
         cat: +d.Obs
     };
 }).then(function(data) {
-    // load sites from geojson file
-    d3.json('data/sites.geojson').then(function(sites) {
-        console.log(data);
-        console.log(sites);
 
-        cached_sites = sites;
-        cached_data = data;
-
+    function addSlider() {
         // add the jQuery slider
         $(function() {
             let minDate = new Date('2017-07-02');
@@ -31,54 +25,98 @@ d3.dsv(',','data/imgCat.csv', function(d) {
                 }
             });
         });
+    }
 
-        for(let i = 0; i < data.length; i++) {
-            for(let j = 0; j < sites.features.length; j++) {
-                // ----- join here -----
-                //create a key value pair in an array for each site
-                if(data[i].seq == sites.features[j].properties.STA_SEQ) {
-                    if('samples' in sites.features[j].properties){
-                        let sample = {'date': data[i].date, 'category': data[i].cat};
-                        sites.features[j].properties.samples.push(sample);
-                    } else {
-                        sites.features[j].properties.samples = [{'date': data[i].date, 'category': data[i].cat}];
+    function drawMap() {
+        // load sites from geojson file
+        d3.json('data/sites.geojson').then(function (sites) {
+            console.log(data);
+            console.log(sites);
+
+            cached_sites = sites;
+            cached_data = data;
+
+            // combine data
+            for (let i = 0; i < data.length; i++) {
+                for (let j = 0; j < sites.features.length; j++) {
+                    // ----- join here -----
+                    //create a key value pair in an array for each site
+                    if (data[i].seq == sites.features[j].properties.STA_SEQ) {
+                        if ('samples' in sites.features[j].properties) {
+                            let sample = {'date': data[i].date, 'category': data[i].cat};
+                            sites.features[j].properties.samples.push(sample);
+                        } else {
+                            sites.features[j].properties.samples = [{'date': data[i].date, 'category': data[i].cat}];
+                        }
                     }
                 }
             }
-        }
 
-        // load map and position
-        let center = [41.5, -72.8];
-        let map = L.map('mapid').setView(center, 8.5);
+            // load map and position
+            let center = [41.5, -72.8];
+            let map = L.map('mapid').setView(center, 8.5);
 
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: 'mapbox/light-v9'
-        }).addTo(map);
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+                maxZoom: 18,
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+                    '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+                    'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                id: 'mapbox/light-v9'
+            }).addTo(map);
+
+            // default style of sites
+            function style(feature) {
+                return {
+                    color: '#1f78b4',
+                    fillColor: '#d9d9d9',
+                    weight: 1,
+                    stroke: 1,
+                    fillOpacity: .8,
+                };
+            }
+
+            L.geoJSON(sites, {
+                pointToLayer: function (feature, latlng) {
+                    return L.circleMarker(latlng);
+                },
+                style: style,
+                onEachFeature: onEachFeature
+            }).addTo(map);
+
+            function onEachFeature(feature, layer) {
+                // display name of site
+                let siteName = feature.properties.Station_Name;
+                let siteCat;
+                let siteDate;
+                layer.bindPopup("<h3>" + siteName + "</h3>" + "Date: " + siteDate + "<br>" + "Category: " + siteCat);
+            }
+
+            addSlider();
+            //updateMap(sites);
+        });
+    }
+
+    /*function updateMap(sites) {
 
         // get colors for categories on map
         function getColor(d) {
-            if(d == 1) {
+            if (d == 1) {
                 return '#ffffcc';
-            } else if(d == 2) {
+            } else if (d == 2) {
                 return '#c7e9b4';
-            } else if(d == 3) {
+            } else if (d == 3) {
                 return '#7fcdbb';
-            } else if (d == 4){
+            } else if (d == 4) {
                 return '#41b6c4';
-            } else if (d == 5){
+            } else if (d == 5) {
                 return '#2c7fb8';
-            } else if (d == 6){
+            } else if (d == 6) {
                 return '#253494';
             } else {
                 return '#d9d9d9'
             }
         }
 
-        // samples[0] just for testing
         function style(feature) {
             return {
                 color: '#1f78b4',
@@ -89,17 +127,6 @@ d3.dsv(',','data/imgCat.csv', function(d) {
             };
         }
 
-        /*L.geoJSON(sites,{
-            pointTOLayer: function(feature,latlng){
-                return L.circleMarker(latlng,style);
-            }
-        }).addTo(map);*/
-
-        // add data from geojson to map
-        //L.geoJSON(sites, {style: style}).addTo(map);
-
-        //let geojson;
-
         // display information of each site
         function onEachFeature(feature, layer) {
             // display name of site
@@ -108,7 +135,7 @@ d3.dsv(',','data/imgCat.csv', function(d) {
             //let siteDate = feature.properties.samples[5].date;
             let siteCat;
             let siteDate;
-            layer.bindPopup("<h3>"+ siteName + "</h3>" + "Date: " + siteDate + "<br>" + "Category: " + siteCat);
+            layer.bindPopup("<h3>" + siteName + "</h3>" + "Date: " + siteDate + "<br>" + "Category: " + siteCat);
 
             let getData = [];
 
@@ -118,27 +145,19 @@ d3.dsv(',','data/imgCat.csv', function(d) {
                 console.log(sliderDate);
 
                 // attempt to match date from csv with current date on slider
-                for(let i = 0; i < sites.features.length; i++) {
-                    for(let j = 0; j < sites.features[i].properties.samples.length; j++) {
+                for (let i = 0; i < sites.features.length; i++) {
+                    for (let j = 0; j < sites.features[i].properties.samples.length; j++) {
                         let cat = sites.features[i].properties.samples[j].category;
                         let date = sites.features[i].properties.samples[j].date;
 
-                        if(date == sliderDate) {
+                        if (date == sliderDate) {
                             getData.push(date, cat);
                         }
                     }
                 }
-                console.log(getData);
             });
         }
+    }*/
 
-        L.geoJSON (sites,{
-            pointToLayer: function(feature,latlng){
-                return L.circleMarker(latlng);
-            },
-            style:style,
-            onEachFeature: onEachFeature
-        }).addTo(map);
-
-    });
+    drawMap();
 });
